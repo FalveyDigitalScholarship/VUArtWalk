663d756ad67dfeb71b5c2c0a459f7d3bca26f1c1
Erica Y. Hayes <ericay.hayes@gmail.com>
2024-07-11 11:51:47 -0400

    upload files

---
+++ README.md
@@ -0,0 +1,6 @@
+# VU Artwalk Map
+
+This is the repository for the Villanova Artwalk Map.
+
+## License
+MIT
---
+++ scripts/constants.js
@@ -0,0 +1,25 @@
+var constants = {
+	// Storymapap Info
+  _mapTitle: 'Storymap Title',
+  _mapSubtitle: 'Storymap Subtitle',
+  _mapLogo: 'Storymap Logo',
+  // Credits
+	_authorName: 'Author Name',
+	_authorURL: 'Author Email or Website',
+  _codeCredit: 'Code Credit',
+  _webDeveloperURL: 'Web Development Assistant',
+	_githubRepo: 'GitHub Repo',
+  _projectWebsite: 'Project website',
+	// Map Settings
+	_tileProvider: 'Basemap Tiles',
+	_zoomControls: 'Zoom Controls',
+  _narrativeWidth: 'Narrative Width',
+  _imgContainerHeight: 'Media Container Height',
+  _pixelsAfterFinalChapter: 'Pixels After Final Chapter',
+  _narrativeBackground: 'Narrative Background Color',
+  _narrativeText: 'Narrative Text Color',
+  _narrativeActive: 'Active Chapter Background Color',
+  _narrativeLink: 'Narrative Link Color',
+  _googleAnalytics: 'Google Analytics Tracking ID',
+  _enableLightbox: 'Enable Lightbox for Images'
+};
